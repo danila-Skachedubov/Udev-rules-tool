19a694ad3e3cb3c0b32ed583e033a4de9dadc4af
Danila Skachedubov <skachedubovda@basealt.ru>
2024-02-11 10:27:27 +0400

    Add shebang

--- udevpy/udevpy.py
+++ udevpy/udevpy.py
@@ -1,8 +1,15 @@
+#!/usr/bin/python3
+
 import sys
 import json
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         print("Usage: <action> <key1=value1> <key2=value2> ...")
         sys.exit(1)
+
+    subsystem = sys.argv[1]
+    authorized = sys.argv[2]
+    options = {arg.split('=')[0]: arg.split('=')[1] for arg in sys.argv[3:]}
+    print(options)
