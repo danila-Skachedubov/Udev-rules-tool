24fe569776ff0c19f83e4c5a4921e126c33e307d
Danila Skachedubov <skachedubovda@basealt.ru>
2024-02-19 16:41:32 +0400

    Importing libraries

---
+++ udevpy/main_app.py
@@ -0,0 +1,4 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from udevpy import process_parameters
+import sys
+import json
