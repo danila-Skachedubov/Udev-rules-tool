58a1ac157700a37cf676af122356345f7efc9fcd
Danila Skachedubov <skachedubovda@basealt.ru>
2024-02-20 15:56:46 +0400

    Adding parameters for actions with devices.

--- udevpy/main_app.py
+++ udevpy/main_app.py
@@ -3,6 +3,7 @@ from udevpy import process_parameters
 import sys
 import json
 
+
 class UdevRuleConfigurator(QMainWindow):
     def __init__(self):
         super(UdevRuleConfigurator, self).__init__()
@@ -11,16 +12,23 @@ class UdevRuleConfigurator(QMainWindow):
         self.setCentralWidget(central_widget)
 
         self.setWindowTitle('udev_rules')
-        self.setGeometry(100, 100, 300, 200)
+        self.setGeometry(100, 100, 400, 300)
 
         self.device_label = QLabel('Select Device:', central_widget)
         self.device_selector = QComboBox(central_widget)
         self.device_selector.addItems(['USB', 'PCI', 'Network'])
+
         self.action_label = QLabel('Select Action:', central_widget)
         self.action_selector = QComboBox(central_widget)
-        self.action_selector.addItems(['add', 'remove'])
+        self.action_selector.addItems(['add', 'remove', 'Allow Connection', 'Deny Connection', 'Rename', 'Run Script', 'Create Symlink', 'Change Permissions'])
+        self.action_selector.currentIndexChanged.connect(self.show_action_input)
+
+        self.action_input_label = QLabel('Action Input:', central_widget)
+        self.action_input = QLineEdit(central_widget)
+        self.action_input.hide()
 
         self.parameters_layout = QVBoxLayout()
+
         self.add_parameter_button = QPushButton('Add Parameter', central_widget)
         self.add_parameter_button.clicked.connect(self.add_parameter)
 
@@ -32,6 +40,8 @@ class UdevRuleConfigurator(QMainWindow):
         layout.addWidget(self.device_selector)
         layout.addWidget(self.action_label)
         layout.addWidget(self.action_selector)
+        layout.addWidget(self.action_input_label)
+        layout.addWidget(self.action_input)
         layout.addLayout(self.parameters_layout)
         layout.addWidget(self.add_parameter_button)
         layout.addWidget(self.generate_button)
@@ -51,6 +61,7 @@ class UdevRuleConfigurator(QMainWindow):
     def generate_rule(self):
         selected_device = self.device_selector.currentText()
         selected_action = self.action_selector.currentText()
+        action_input_value = self.action_input.text()
 
         parameters = {}
         for i in range(0, self.parameters_layout.count(), 4):
@@ -59,9 +70,19 @@ class UdevRuleConfigurator(QMainWindow):
             if param_name and param_value:
                 parameters[param_name] = param_value
 
-        json_data = json.dumps({'SUBSYSTEM': selected_device, 'ACTION': selected_action, 'parameters': parameters})
+        parameters[selected_action] = action_input_value
+
+        json_data = json.dumps({'SUBSYSTEM': selected_device, 'ACTION': selected_action, **parameters})
 
         process_parameters(json_data)
+
+    def show_action_input(self):
+        selected_action = self.action_selector.currentText()
+        if selected_action in ['Allow Connection', 'Deny Connection', 'Rename', 'Run Script', 'Create Symlink', 'Change Permissions']:
+            self.action_input.show()
+        else:
+            self.action_input.hide()
+
 
 if __name__ == '__main__':
     app = QApplication([])
--- udevpy/udevpy.py
+++ udevpy/udevpy.py
@@ -1,13 +1,6 @@
 import json
 
-def process_parameters(selected_device, selected_action, parameters):
-    data = {
-        'Selected Device': selected_device,
-        'Selected Action': selected_action,
-        'Parameters': parameters
-    }
+def process_parameters(json_data):
+    print(json_data)
 
-    print(f'Processing saved parameters:')
-    print(f'Selected Device: {data["Selected Device"]}')
-    print(f'Selected Action: {data["Selected Action"]}')
-    print(f'Parameters: {json.dumps(data["Parameters"], indent=4)}')
+
