e4c7c046bd9376142dea4c47938787705003cbbd
Danila Skachedubov <skachedubovda@basealt.ru>
2024-03-04 18:41:16 +0400

    Added method for processing incoming json

--- udevpy/udevpy.py
+++ udevpy/udevpy.py
@@ -1,6 +1,19 @@
 import json
 
+
 def process_parameters(json_data):
-    print(json_data)
 
+     try:
+          data = json.loads(json_data)
+          action = data.get('ACTION')
+          subsystem = data.get('SUBSYSTEM')
+          rule = data.get('RULE')
+          parameters = {key: value for key, value in data.items() if key not in ['ACTION', 'SUBSYSTEM', 'RULE']}
 
+          create_udev_rule(action, subsystem, rule, parameters)
+
+     except Exception as exc:
+          print(exc)
+
+def create_udev_rule(action, subsystem, rule, parameters):
+     pass
