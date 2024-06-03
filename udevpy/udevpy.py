1a6c4d86d7750ead6145a4c4ea0a75efc2e9f525
Danila Skachedubov <skachedubovda@basealt.ru>
2024-06-03 18:32:39 +0400

    Added a method for uploading a json file

--- udevpy/udevpy.py
+++ udevpy/udevpy.py
@@ -1,29 +1,26 @@
-#
-# GPOA - GPO Applier for Linux
-#
-# Copyright (C) 2019-2024 BaseALT Ltd.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import json
 import os
 
 class UdevApplier:
     json_dir = "/etc/udev/json"
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.rules = {}
 
     def load_json_files(self):
-        pass
+        for filename in os.listdir(self.json_dir):
+            if filename.endswith(".json"):
+                file_path = os.path.join(self.json_dir, filename)
+                with open(file_path, 'r') as file:
+                    try:
+                        data = json.load(file)
+                        self.rules.update(data)
+                    except json.JSONDecodeError as e:
+                        print(f"Error decoding JSON from file {file_path}: {e}")
+
+
+
+
+if __name__ == '__main__':
+    applier = UdevApplier()
+    applier.load_json_files()
